d74db6267ed632845741ca5e2ad0d5d9fb6ef912
Atelier04.at <alexander.fellner@yahoo.de>
2023-07-11 15:17:43 +0200

    Erklärung von Kommentaren, Zhalentypen und Logischen Typen

---
+++ variablen.py
@@ -0,0 +1,27 @@
+""""
+Dieser Kommetar geht über mehrere Zeilen
+Hier kommt noch ein Text
+Beispiel für mehrzeiliges Kommentar
+"""
+# Das ist ein einzeiliger Kommentar
+""""
+Erklärung zu Byte und Bit
+Ein Byte ist eine Kombination von 8 Bit.
+Ein Bit kann den Wert 0 oder 1 annehmen.
+Beispiel für Byte:
+10001011
+Umrechnung von binär in dezimal an einem Beispiel:
+101(binär) ist 1*2^2+0*2^1+1*2^0=5
+Datentypen:
+1) Zahlentypen
+int ( ganze Zahlen) 1,2,3....
+Wertebereich bei int -2^32-1  ..... 0 .... 2^31-1
+float (Kommazahlen) 1.0, 2.48, 3.14, 7.9432423...
+Komplexer Zahlentyp verwendet  so genannte imaginäre Einheit(j)
+Beispiel: z = 2+ 3 * j
+Für komplexe mathematische Operationen (Rechnen mit großen Zahlen) verwendet man Bibliotheken wie z.B numpy 
+
+"""
+# Boolscher Typ kann nur den Wert True oder False haben.
+wahrheitsvariable = True
+# Beachte True ist ein keyword und darf nicht der Name einer Variablen sein
