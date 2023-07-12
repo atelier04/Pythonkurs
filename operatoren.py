660422b6242b3b75888f48010b0aa167f0cf3b54
Atelier04.at <alexander.fellner@yahoo.de>
2023-07-12 13:10:20 +0200

    Erklärung zu Arithmetischen und Vergleichsoperatoren und Beispiele

---
+++ operatoren.py
@@ -0,0 +1,42 @@
+"""
+Operatoren
+
+Definition: Vorschrift, die auf einen bestimmten Ausdruck angewendet wird, um ein bestimmtes Ergebnis zu bekommen
+"""
+# Arithmetischen Operatoren
+zahl_zwei = 2
+zahl_drei = 3
+# Beispiel fü den + Operator
+print("Die summe von zahl_zwei und zahl_drei ist ", zahl_zwei + zahl_drei)
+# Beispiel für den Divisionsoperator
+zahl_vier = 4
+zahl_fünf = 5
+print("Die Division von zahl_vier und zahl_fünf liefert  ", zahl_fünf / zahl_vier)
+# Modulo Operator
+# Beispiel
+zahl_sechs = 6
+zahl_sieben = 7
+print("Die Modulo Operation von zahl_sechs und zahl_sieben ergibt   ", zahl_sechs % zahl_sieben)
+zahl_sieben = 7
+zahl_sechs = 6
+print("Die Modulo Operation von zahl_sieben und zahl_sechs ergibt   ", zahl_sieben % zahl_sechs)
+print(" Die ganzzahlige Division von zahl_sieben und zahl_zwei ergibt " , zahl_sieben // zahl_zwei )
+# Vergleichsoperatoren , liefern immer einen logischen Ausdruck
+# Bei einfachen Datentypen werden direkt die Inhalte(Werte) verglichen
+# Beispiel für ganze Zahlen
+zahl_acht = 8
+zahl_neun = 9
+print(" Der Vergleich von zahl_acht und zahl_neun ergibt ",zahl_acht == zahl_neun)#
+# Beispiel für Strings
+wort_eins = "Text1"
+wort_zwei = "text1"
+print("Der Vergleich von wort_eins mit wort_zwei ergibt", wort_eins == wort_zwei)
+# Beispiel für Ungleich !=
+print("Der Vergleich mit Ungleich(!=)  von wort_eins mit wort_zwei ergibt ",wort_eins!=wort_zwei)
+# Beispiel zu and
+logisch_var1= True
+logisch_var2 = False
+print("Die and Operation von logisch_var1 mit logisch_var2 ergibt  ",logisch_var1 and logisch_var2)
+logisch_var1 =  True
+logisch_var2 = False
+print("Die or Operation von logisch_var1 mit logisch_var2 ergibt  ",logisch_var1 or logisch_var2)
