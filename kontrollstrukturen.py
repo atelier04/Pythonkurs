b487f26645117bba01ec2373a5921373b497b611
Atelier04.at <alexander.fellner@yahoo.de>
2023-07-19 12:57:52 +0200

    Switch Case Und Übungsbeispiel dazu

--- kontrollstrukturen.py
+++ kontrollstrukturen.py
@@ -1,5 +1,5 @@
 # wetter ist gut
-wetter_gut = False
+"""wetter_gut = False
 sommer = False
 if wetter_gut:
     print("Wetter ist gut")
@@ -7,8 +7,7 @@ if wetter_gut:
     if sommer:
         print("Hurra Sommer")
         print("Cool, Endlich warm, kein Frieren")
-
-print("Nach if von wetter_gut, wird auf jeden Fall ausgegeben")
+        print("Nach if von wetter_gut, wird auf jeden Fall ausgegeben")
 # Beispiel zu if else
 price = 999
 if price > 1200:
@@ -27,3 +26,33 @@ else:
 # und erstelle einen else block nach dem if block von sommer, und auch im diagramm darstellen
 # Wie sieht ein if mit 2 Bedingungen aus? Welche Fälle gibt es?
 # Ausblick für nächstes mal elif werden wir genau besprechen ( siehe Skriptum)
+#Struktogramme , Ablaufdiagramme zuim Visualisieren des Programmsablauf
+"""
+#Übungsbeispiel
+# Benutzer soll im Programm eine Zahl zwischen 1 und 3 eingeben. Wenn die Zahl nicht 1 oder 2 oder 3 ist, soll ausgegeben werden,
+# "Bitte gib nur eine zahl zwischen 1 und 3 ein. Wenn 1,2, 3 eingegeben wurde, soll die Ausgabe sein " Das Doppelte von dem, was du eingegeben hast, ist "
+# und dann das Ergebnis .
+zahl = input("Bitte gib eine Zahl ein zwischen 1 und 3. ")
+print("Du hast eingeben "+ zahl)
+""""
+switch(Ausdruck):
+    case Wert1:
+        Anweisung1
+        Anweisung2
+        Anweisung3
+        break
+    case Wert2:
+         AnweisungWert2
+         break
+    case Wert3
+        Anweisung3
+        break
+    default:
+        Anweisungsonst
+
+print("Nach switch")
+
+
+"""
+
+
---
+++ kontrollstrukturenlösungen.py
@@ -0,0 +1,14 @@
+wetter_gut = True
+sommer = True
+if wetter_gut:
+    print("Wetter ist gut")
+    print("Cool, kann raus gehen")
+    if sommer:
+        print("Hurra Sommer")
+        print("Cool, Endlich warm, kein Frieren")
+
+    else:
+        pass
+
+        print("Wetter ist nicht gut, Muss drinnen bleiben")
+print("Nach if von wetter_gut, wird auf jeden Fall ausgegeben")
