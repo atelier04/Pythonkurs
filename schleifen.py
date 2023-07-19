8e0aeaa4f6fc544361fc3a7f92299c80560e1fc9
Atelier04.at <alexander.fellner@yahoo.de>
2023-07-19 12:56:27 +0200

    For In Schleife und Übungsbeispiel

---
+++ schleifen.py
@@ -0,0 +1,14 @@
+# Schleife:
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+# Wenn Code sich wiederholt( sich nur mehr eine Kleinigkeit ändert), dann Schleife machen
+for zahl in range(16):
+    print(zahl**2)
+#Übungsbeispiel Berechne mit einer for in schleife die summe der ersten 100 Zahlen und lasse die Summe ausgeben
+# Der Benutzer soll eine Zahl eingeben, die soll größer als 1 sein, ansonsten meldung an den benutzer. Wenn größer als Eins, dann
+# soll die Summe bis zu der zahl berechnet werden.
+# Beispiel Benutzer gibt 10 ein, dann soll 1+2+3+4+5+6+7+8+9+10.... +100  Trick(Formel): 100/2 *101=5050 10*11/2=55
