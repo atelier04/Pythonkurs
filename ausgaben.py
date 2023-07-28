34c8a49e4d3fa5d51fb47e71e4d475dc8270cf06
Atelier04.at <alexander.fellner@yahoo.de>
2023-07-28 12:41:17 +0200

    Geschachtelte Schleifen

---
+++ ausgaben.py
@@ -0,0 +1,8 @@
+artikel1=23.789
+artikel2=0.039
+artikel3=199.8
+artikel4=23
+artikel5=2324.17
+print("Artikel %6.2f %6.2f %6.2f %6.2f %6.2f" % (23.789,0.039,199.8,23,2324.17))
+print("Artikel {1:6.2f} {2:6.2f} {3:6.2f} {4:6.2f} {5:6.2f}".format(artikel1, artikel2, artikel3, artikel4, artikel5))
+
