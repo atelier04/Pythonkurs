0e8e464ec0ff2c43efc02b9d82a1f9b84d0d64bd
Atelier04.at <alexander.fellner@yahoo.de>
2023-07-28 12:42:13 +0200

    Revert "Geschachtelte Schleifen"
    This reverts commit 34c8a49e4d3fa5d51fb47e71e4d475dc8270cf06.

--- schleifen.py
+++ schleifen.py
@@ -9,7 +9,6 @@
 start=4
 end=1
 count=0
-
 for zahl in range(start,end,-2):
 
     count=count+1
@@ -18,26 +17,23 @@ for zahl in range(start,end,-2):
     print(zahl**2)
 else:
     print("In else von for in")
-
 #Übungsbeispiel Berechne mit einer for in schleife die summe der ersten 100 Zahlen und lasse die Summe ausgeben
 # Der Benutzer soll eine Zahl eingeben, die soll größer als 1 sein, ansonsten meldung an den benutzer. Wenn größer als Eins, dann
 # soll die Summe bis zu der zahl berechnet werden.
 # Beispiel Benutzer gibt 10 ein, dann soll 1+2+3+4+5+6+7+8+9+10.... +100  Trick(Formel): 100/2 *101=5050 10*11/2=55
 for x in range(20,1,-3):
-    print("Am Beginn  der Schleife")
     print("Die zahl ist ",x)
     if x == 11:
 
-        print("in if von x=11, die zahl ist ", x)
-        continue
-    print("Der Code nach Continue")
-    print("Innerhalb der Schleife  ausserhalb von x gleich 11, die zahl ist ", x)
+        print("in if von x=11")
+        break
+
+    print("Innerhalb der Schleife  ausserhalb von x gleich 11 ")
 else:
     print(" innerhalb von else der for in Schleife ")
 print("nach break")
 # while schleife
 # while bedingung:
-
 x = 5
 print ("-----------------------------------------------------------")
 while x < 10:
@@ -54,56 +50,6 @@ else:
     print("In else von while, die zahl x ist ", x)
 print("ausserhalb von while else ",x)
 
-# Passwortabfrage Beispiel für eine fußgesteuerte Schleife
-"""
-passwort = "secret"
-eingabe = input("Bitte um das Passwort\n")
-# Kopfgesteuerte Schleife
-while eingabe != passwort:
-    print("Deine Eingabe ist inkorrekt")
-    eingabe = input("Bitte um das Passwort wieder\n")
-    if eingabe == passwort:
-        print("Deine Eingabe ist korrekt, du darfst die Applikation verwenden")
-        #break
-if eingabe == passwort:
-    print("Du bist in der Applikation. Congratulations!!")
 
-# Für graphische Applikationen gibt es Bibliotheken z.B tkinter
-# Übungsbeispiel Benutzer soll Passwort und Benutzernamen eingeben. Beide sollen korrekt sein
-# Wenn nein, dann wird der Benutzer aufgefordert beide wieder einzugeben, solange bis beide korrekt sind.
 
-# In Python keine Fußgesteuerten Schleifen
-"""
-"""
-Geht nur in Programmiersprachen wie Java, C++, C#, Javascript
-do {
-    eingabe = input("Bitte um das Passwort \n")
-    if eingabe == passwort:
-        print("Du bist in der Applikation. Congratulations!!")
-        break
-    
-}
-while(eingabe!=passwort);
-"""
-
-print("Die Eingabe ist irgendetwas",sep="", end="")
-print("In derselben Zeile")
-# Übungsbeispiel (Bonus, Bei Gelingen gibts Cafe)
-"""
-          *
-         ***
-        *****
-       *******
-      #########
-"""
-startaussen=1
-endaussen=10
-
-startinnen=15
-endinnen = 3
-for zahlaussen in range(1,10,1):
-    print(f"Bin in der äusseren Schleife mit zahlaussen ist: {zahlaussen}")
-    for zahlinnen in range(15,3,-1):
-        print(f"Bin in der inneren Schleife mit zahlinnen ist: {zahlinnen}" )
-    print(f"Bin wieder in der äusseren Schleife  mit zahlaussen ist: {zahlaussen} ")
 
