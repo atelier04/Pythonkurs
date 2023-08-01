3b30bd1cb5e12546f382b0a222eca892e2ebfde2
Atelier04.at <alexander.fellner@yahoo.de>
2023-08-01 14:51:37 +0200

    Weitere Lösung für Sternendreieck

--- schleifenlösung.py
+++ schleifenlösung.py
@@ -1,24 +1,24 @@
-aussen=5
+aussen = 5
 "Dreieck normal"
 for zeile in range(aussen):
 
-    for space in range(aussen-(zeile+1)):
+    for space in range(aussen - (zeile + 1)):
         print(" ", end="")
-    for stern in range(0,2*(zeile+1)-1):
-        print("*",end="")
+    for stern in range(0, 2 * (zeile + 1) - 1):
+        print("*", end="")
 
     print()
 "Dreieck verkehrt"
 print("-----------Dreieck verkehrt------")
 for zeile in range(aussen):
-    for space in range(1,zeile+1):
-        print(" ",end="")
-    for stern in range(0,2*(aussen-zeile)-1):
+    for space in range(1, zeile + 1):
+        print(" ", end="")
+    for stern in range(0, 2 * (aussen - zeile) - 1):
         print("*", end="")
     print()
 
-for x in range(1,5):
-    if x==3:
+for x in range(1, 5):
+    if x == 3:
         print("x ist 3")
 else:
     print("In else of for")
@@ -28,3 +28,27 @@ while x < 10:
     x += 1
 else:
     print("x is not less than 10")
+
+# Sternendreieck
+# ähnliche Lösung
+startzeile = 1
+endzeile = 6
+for zeile in range(startzeile, endzeile,1):
+
+    for space in range(1, endzeile-(zeile-1)):
+        print("#", end="")
+
+    for star in range(0,2 * zeile -1,1):
+                print("*",end="")
+
+    print()
+
+
+
+
+#ungerade zahlen in mathematik
+# Formel: 2 * zeile - 1
+
+
+
+
